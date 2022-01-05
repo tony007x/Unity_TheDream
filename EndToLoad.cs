89d8d040842061034bf58df3d6d5b483c89b123c
LordTony <66866324+tony007x@users.noreply.github.com>
2022-01-05 16:49:16 +0700

    Add files via upload

---
+++ EndToLoad.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+ 
+public class EndToLoad : MonoBehaviour
+{
+ 
+     VideoPlayer video;
+     public GameObject ClassVideo;
+     public GameObject ClasswithoutVideo;
+ 
+    void Start()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        StartCoroutine("WaitForMovieEnd");
+    }
+ 
+ 
+    public IEnumerator WaitForMovieEnd()
+    {
+        while (video.isPlaying)
+        {
+            yield return new WaitForEndOfFrame();
+         
+        }
+        OnMovieEnded();
+    }
+ 
+     void OnMovieEnded()
+    {
+        if(ClassVideo.activeInHierarchy == true)
+        {
+            ClassVideo.SetActive(false);
+            ClasswithoutVideo.SetActive(true);
+        }
+        
+    }
+}
+ 
